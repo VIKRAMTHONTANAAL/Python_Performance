0959cd02ddae070114ef40e0b37bee182a37c543
vikrammthontanaal <thontanaalvikram@gmail.com>
2025-02-07 20:24:49 +0530

    nested task and multiple task set

--- TaskSet/httpcat.py
+++ TaskSet/httpcat.py
@@ -8,6 +8,7 @@ class MyHTTPCat(TaskSet):
     def get_status(self):
         response = self.client.get("/200")
         print("Get Status of 200")
+        self.interrupt(reschedule=False)
 
     @task
     def get_random_status(self):
@@ -15,10 +16,22 @@ class MyHTTPCat(TaskSet):
         random_url = "/" + str(random.choice(status_codes))
         res = self.client.get(random_url)
         print("Random http status " + random_url)
+        self.interrupt(reschedule=False)
+
+   
+class MyAnotherHTTPCat(TaskSet):
+
+    @task
+    def get_500_status(self):
+        res = self.client.get("/500")
+        print("Get status of 500")
+        self.interrupt(reschedule=False)
+
+        #self.interrupt(reschedule=False)
 
 
 
 class MyLoadTest(HttpUser):
     host = "https://http.cat"
-    tasks = [MyHTTPCat]
+    tasks = [MyHTTPCat, MyAnotherHTTPCat]
     wait_time = constant(1)
